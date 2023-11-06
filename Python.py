aed317a641556eea8a87a7f1120770f0d98b6dc3
faqih703 <faqihudin703@gmail.com>
2023-11-06 08:32:56 +0700

    faqih703

--- Python.py
+++ Python.py
@@ -5,3 +5,15 @@
 # scanf/cin = input(""), printf/cout = print, string = str,
 # tidak usah double, cukup int & float, int = tidak memakai koma, float = memakai koma
 # untuk comment memakai # bukan //
+# echo "# git_nic" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/faqihudin703/git_nic.git
+# git push -u origin main
+# git remote add origin https://github.com/faqihudin703/git_nic.git
+# git branch -M main
+# git push -u origin main
+
+
